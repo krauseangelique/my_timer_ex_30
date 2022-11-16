77eaf03bc3239e31d2b3d490fb8d7548927959c9
angelique KRAUSE <krause_angelique@hotmail.com>
2022-11-16 16:03:43 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log('hello world');
+// datatime 20 secondes et je décompte 20 sec
+const allBtns = document.querySelectorAll("[data-time]");
+
+console.log(allBtns[0].dataset.time);
+
+function handleTwentySec() {
+  document.querySelector(".display__time-left").textContent =
+    allBtns[0].dataset.time;
+}
+handleTwentySec();
+
