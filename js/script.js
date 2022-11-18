b1652b6fe0d10f940dad2ccdc8bb1cca438d3b6b
angelique KRAUSE <krause_angelique@hotmail.com>
2022-11-18 11:41:15 +0100

    remove js

--- js/script.js
+++ js/script.js
@@ -2,50 +2,102 @@ console.log('hello world');// mon fichier script.js est bien appelé !
 
 // datatime nbre secondes et je décompte nbre secondes
 const allBtns = document.querySelectorAll("[data-time]");
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+
+
 
 /* documentation sur le décompteur en anglais : countdown is on (... est lancé !)
 https://www.delftstack.com/fr/howto/javascript/count-down-timer-in-javascript/
 
 */
 
+function timer(seconds){
+  clearInterval(countdown);
+  const date = Date.now();
+  const then = date + seconds * 1000;
 
-function handleSec(e) {
+  displayTimeLeft(seconds);
+  displayEndTime(then);
 
-  // mon bouton cliqué
-  let btnClicked = e.target;
-  let btnClickedData = btnClicked.dataset.time;
+  countdown = setInterval(()=>{
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
 
-  console.log(btnClickedData);
-
-  // diminution du compteur jusqu'à 0 seconde
- btnClickedData = btnClickedData - 1;
- if (btnClickedData < 0) { clearInterval(handleSec)};
-  
-  // transformation des secondes en pattern 01:00:00 pour 1h=3600sec
-  let date = new Date(null);
-  date.setSeconds(btnClickedData);
-
-  let hhmmssFormat = "";
-
-
-    if(btnClickedData < 3600){
-      hhmmssFormat = date.toISOString().substring(14,19);
-      console.log(hhmmssFormat);
-
-    }else{
-      hhmmssFormat = date.toISOString().substring(11,19);
-      console.log(hhmmssFormat);
+    if(secondsLeft < 0){
+      clearInterval(countdown)
+      return;
     }
 
-  document.querySelector('.display__time-left').textContent = hhmmssFormat; // mise en place du pattern avec textContent le compteur est mis à sa place
+    displayTimeLeft(secondsLeft);
+  }, 1000)
+}
+
+function displayTimeLeft(seconds){
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : "" }${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
+}
+
+
+function displayEndTime(timestamp){
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour -12 : hour;
+  const minutes = end.getMinutes();
+  displayEndTime.textContent = `Revenir à ${adjustedHour}:${minutes < 10 ? "0" : ""}${minutes}`;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+
+allBtns.forEach(btn => {
+  btn.addEventListener('click', startTimer);
+});
+document.customForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  console.log(mins);
+  timer(mins * 60);
+  this.reset();
+});
+// function handleSec() {
+  //   clearInterval(countdown);
+  //   let btnClickedData = this.dataset.time;
+  //   let date = new Date();
+  //   date.setSeconds(btnClickedData);
+  
+  //   if (btnClickedData < 3600){
+    //     hhmmssFormat = date.toISOString().substring(14,19);
+//     console.log(hhmmssFormat);
+
+//   } else{
+//     hhmmssFormat = date.toISOString().substring(11,19);
+//     console.log(hhmmssFormat);
+//   }
+  
+  // document.querySelector('.display__time-left').textContent = hhmmssFormat; // mise en place du pattern avec textContent le compteur est mis à sa place
+ 
+  
+  // document.querySelector('.display__end-time').textContent = 'Revenir à '+ then ;
+
+  
+  // transformation des secondes en pattern 01:00:00 pour 1h=3600sec
+
+
+
+   
+
+  // + (date + (setSeconds)); // heure courante + l'espace timer
+  
 
   // TRANSFORMATION en décompteur
 
   
-}
 
 // Appel de la fonction dans une forEach pour AFFICHER au click sur le bandeau de bouton, le compteur
-allBtns.forEach(btn => {
-  btn.addEventListener('click', handleSec);
-});
-
